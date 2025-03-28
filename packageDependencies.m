9a3441911abffdef8540bdda7cbf86db7d061347
nickersonm <github-account@nickersonm.com>
2025-03-28 13:47:10 -0400

    Added dependency finding tools

--- packageDependencies.m
+++ packageDependencies.m
@@ -8,11 +8,7 @@ deps = unique(deps(:));
 
 % Assemble package
 [~, package] = fileparts(scriptName);
-<<<<<<< HEAD
 package = "'" + string(package) + "'_and_dependencies.zip";
-=======
-package = string(package) + "_and_dependencies.zip";
->>>>>>> 54d215d43c3f26fd59fd89a403fd874e2bb21b97
 zip(package, deps);
 
 fprintf("\nPackaged %i dependencies of '%s' into '%s'\n", numel(deps), scriptName, package);
